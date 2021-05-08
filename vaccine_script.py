a73d0f1d6b5d9a8570a6f2fbbe8a96e432271372
AxelKoerner <91koax1bif@hft-stuttgart.de>
2021-05-08 17:11:10 +0200

    innitial commit

---
+++ vaccine_script.py
@@ -0,0 +1,127 @@
+import selenium
+from selenium import webdriver
+from selenium.webdriver.firefox.options import Options 
+from selenium.webdriver.common.keys import Keys
+from selenium.webdriver.common.by import By
+from selenium.webdriver.support.select import Select
+import time 
+
+options = Options()
+options.binary_location = r"C:\Users\axel_\AppData\Local\Mozilla Firefox\firefox.exe"
+browser = webdriver.Firefox(options=options, executable_path=r'C:\Users\axel_\AppData\Local\Mozilla Firefox\geckodriver.exe')
+
+
+
+##################################define different functions for setting Bundesland and place to be vaccinated ####################################
+
+
+def selectBundesland():
+  browser.get("https://www.impfterminservice.de/impftermine")                                           #starts vaccine site 
+  time.sleep(1)                                                                                         #sleep to load site
+  ddBundesland = browser.find_elements_by_class_name("select2-selection.select2-selection--single")[0]  #select drop Down List
+  ddBundesland.send_keys(Keys.RETURN)                                                                   #start drop Down List
+  ddBundesland.send_keys(Keys.RETURN)                                                                   #choose Baden Württemberg
+
+def submitEntries():
+  submit = browser.find_element_by_class_name("btn.kv-btn.btn-magenta.text-uppercase.d-inline-block")
+  submit.send_keys(Keys.RETURN)
+  time.sleep(2)
+  anspruchPruefen = browser.find_elements_by_class_name("ets-radio-control")[1]
+  anspruchPruefen.click()  
+  
+def waiblingen():
+  impfzentrum = browser.find_elements_by_class_name("select2-selection.select2-selection--single")[1]    #select drop down element for vaccine centre 
+  impfzentrum.send_keys(Keys.RETURN)
+  i = 0
+  while i < 9:
+    impfzentrum.send_keys(Keys.ARROW_DOWN)
+    i += 1
+  impfzentrum.send_keys(Keys.RETURN)
+  time.sleep(1)
+
+def stuttgartKlinikum():
+  impfzentrum = browser.find_elements_by_class_name("select2-selection.select2-selection--single")[1]    #select drop down element for vaccine centre 
+  impfzentrum.send_keys(Keys.RETURN)
+  i = 0
+  while i < 4:
+    impfzentrum.send_keys(Keys.ARROW_DOWN)
+    i += 1
+  impfzentrum.send_keys(Keys.RETURN)
+  time.sleep(1)  
+
+def stuttgartRobertBosch():
+  impfzentrum = browser.find_elements_by_class_name("select2-selection.select2-selection--single")[1]    #select drop down element for vaccine centre 
+  impfzentrum.send_keys(Keys.RETURN)
+  i = 0
+  while i < 5:
+    impfzentrum.send_keys(Keys.ARROW_DOWN)
+    i += 1
+  impfzentrum.send_keys(Keys.RETURN)
+  time.sleep(1)
+
+def ludwigsburg():
+  impfzentrum = browser.find_elements_by_class_name("select2-selection.select2-selection--single")[1]    #select drop down element for vaccine centre 
+  impfzentrum.send_keys(Keys.RETURN)
+  i = 0
+  while i < 10:
+    impfzentrum.send_keys(Keys.ARROW_DOWN)
+    i += 1
+  impfzentrum.send_keys(Keys.RETURN)
+  time.sleep(1)
+
+def ilsfeld():
+  impfzentrum = browser.find_elements_by_class_name("select2-selection.select2-selection--single")[1]    #select drop down element for vaccine centre 
+  impfzentrum.send_keys(Keys.RETURN)
+  i = 0
+  while i < 20:
+    impfzentrum.send_keys(Keys.ARROW_DOWN)
+    i += 1
+  impfzentrum.send_keys(Keys.RETURN)
+  time.sleep(1)
+
+def rotAmSee():
+  impfzentrum = browser.find_elements_by_class_name("select2-selection.select2-selection--single")[1]    #select drop down element for vaccine centre 
+  impfzentrum.send_keys(Keys.RETURN)
+  i = 0
+  while i < 22:
+    impfzentrum.send_keys(Keys.ARROW_DOWN)
+    i += 1
+  impfzentrum.send_keys(Keys.RETURN)
+  time.sleep(1) 
+
+
+################################################################################## main loop ###################################################################################
+
+
+while True:
+  selectBundesland()
+  waiblingen()
+  submitEntries()
+  keinErfolg = browser.find_element_by_xpath("/html/body/app-root/div/app-page-its-login/div/div/div[2]/app-its-login-user/div/div/app-corona-vaccination/div[3]/div/div/div/div[2]/div/div/div")
+  time.sleep(5)
+  if keinErfolg == False:
+      break
+
+  selectBundesland()
+  ludwigsburg()
+  submitEntries()
+  keinErfolg = browser.find_element_by_xpath("/html/body/app-root/div/app-page-its-login/div/div/div[2]/app-its-login-user/div/div/app-corona-vaccination/div[3]/div/div/div/div[2]/div/div/div")
+  time.sleep(5)
+  if keinErfolg == False:
+      break
+  
+  selectBundesland()
+  ilsfeld()
+  submitEntries()
+  keinErfolg = browser.find_element_by_xpath("/html/body/app-root/div/app-page-its-login/div/div/div[2]/app-its-login-user/div/div/app-corona-vaccination/div[3]/div/div/div/div[2]/div/div/div")
+  time.sleep(5)
+  if keinErfolg == False:
+      break
+
+  selectBundesland()
+  rotAmSee()
+  submitEntries()
+  keinErfolg = browser.find_element_by_xpath("/html/body/app-root/div/app-page-its-login/div/div/div[2]/app-its-login-user/div/div/app-corona-vaccination/div[3]/div/div/div/div[2]/div/div/div")
+  time.sleep(5)
+  if keinErfolg == False:
+      break
